a26f5e1c19a5d6f25e0ee87cacbd3987477060e1
FanxingMeng <fanxing66666@gmail.com>
2020-06-05 23:55:07 +0800

    Updates

---
+++ precache-manifest.5d27740d8ebc8db50c2e2decc51a8915.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ea5176e7d22727805649a3151c5cc3",
+    "url": "/mega-web-test/index.html"
+  },
+  {
+    "revision": "0ba75c4517bc9e0bf9dd",
+    "url": "/mega-web-test/static/css/2.729fb9b6.chunk.css"
+  },
+  {
+    "revision": "3f3bfa9e66f171917421",
+    "url": "/mega-web-test/static/css/main.94722649.chunk.css"
+  },
+  {
+    "revision": "0ba75c4517bc9e0bf9dd",
+    "url": "/mega-web-test/static/js/2.282910aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mega-web-test/static/js/2.282910aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f3bfa9e66f171917421",
+    "url": "/mega-web-test/static/js/main.17a0a000.chunk.js"
+  },
+  {
+    "revision": "faa2dc732352d7bf83e4",
+    "url": "/mega-web-test/static/js/runtime-main.1119d68b.js"
+  }
+]);
